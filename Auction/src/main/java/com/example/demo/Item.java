7f7125e16eb4c9baa52d67b32b49c490e0168ff9
shoo <Souza_97@hotmail.se>
2021-03-11 10:55:59 +0100

    lagt instansvariablar i Item

--- Auction/src/main/java/com/example/demo/Item.java
+++ Auction/src/main/java/com/example/demo/Item.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
 public class Item {
+    private int id;
+    private String name;
+    private String description;
+    private Category category;
+    private int startingBid;
+    private String endTime;
+    private int enabled;
+    private String picture;
+
 }
