1745442247788116cd17bcaf82cdcf11689b24db
shoo <Souza_97@hotmail.se>
2021-03-11 10:48:13 +0100

    Add class Bid

---
+++ Auction/src/main/java/com/example/demo/Bid.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Bid {
+    private int id;
+    private User bidder;
+    private Item item;
+    private int price;
+}
--- Auction/src/main/java/com/example/demo/User.java
+++ Auction/src/main/java/com/example/demo/User.java
@@ -7,6 +7,6 @@ public class User {
     private String password;
     private String role;
 
- 
+
 
 }
