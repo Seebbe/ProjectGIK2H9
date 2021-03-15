d2f8290db961f1c4d2baff09fee3043c4491d58a
Kevin Ramlinger <Kevin023@live.se>
2021-03-15 16:02:03 +0100

    Admin sida men får ej in items

--- Auction/src/main/java/com/example/demo/controllers/AdminController.java
+++ Auction/src/main/java/com/example/demo/controllers/AdminController.java
@@ -1,12 +1,17 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Item;
 import com.example.demo.models.User;
+import com.example.demo.repositories.ItemRepository;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin/")
@@ -14,11 +19,26 @@ public class AdminController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    ItemRepository itemRepository;
+
+
     @GetMapping("")
     public String index(Model model) {
         User loggedInUser = userRepository.findByEmail(MainController.getLoggedInUser());
         model.addAttribute("admin", loggedInUser);
         model.addAttribute("meddelande", "Jag är en admin!");
         return "test";
+    }
+
+    @GetMapping("/items")
+    public String GetAllItems(Model model, @RequestParam(required = false) Integer id){
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+
+        List<User> users = userRepository.findAll();
+        model.addAttribute("users", users);
+
+        return "admin";
     }
 }
--- Auction/src/main/java/com/example/demo/controllers/MainController.java
+++ Auction/src/main/java/com/example/demo/controllers/MainController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
@@ -25,8 +28,11 @@ public class MainController {
 
     //PASSWORD = 123
     //return "test";
-
-}
+    }
+    @GetMapping(value = "/Itemsite")
+        public String GetItemSite() {
+        return "Item";
+    }
 
     public static String getLoggedInUser(){
         String userName = "";
@@ -41,23 +47,6 @@ public class MainController {
 
         //hårdkodar en user som returneras
             userName =  "seller@user.com";
-
-        return userName;
-    }
-
-    public static String getLoggedInAdmin(){
-        String userName = "";
-        /*Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (principal instanceof UserDetails) {
-            userName = ((UserDetails)principal).getUsername();
-        } else {
-            userName = "-";
-        }
-
-         */
-
-        //hårdkodar en user som returneras
-        userName =  "admin@admin.se";
 
         return userName;
     }
