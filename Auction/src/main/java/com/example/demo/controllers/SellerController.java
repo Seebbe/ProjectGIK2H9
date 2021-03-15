af99e63a7369317dd2e61d975efa17f0a2ab258c
Sebastian <bassenilsson93@gmail.com>
2021-03-15 13:01:38 +0100

    addAUktion

--- Auction/src/main/java/com/example/demo/controllers/SellerController.java
+++ Auction/src/main/java/com/example/demo/controllers/SellerController.java
@@ -18,43 +18,44 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("/seller/")
+
+@RequestMapping("/seller")
 public class SellerController {
     @Autowired
     ItemRepository itemRepository;
     @Autowired
     UserRepository userRepository;
 
-    @RequestMapping("/seller")
+    @GetMapping("/add")
     public String home(Model model){
 
         return "seller";
     }
-    @RequestMapping("/addauktion")
+    @GetMapping("/addauktion")
     public String addAuktion(@RequestParam(defaultValue = "-1") String name,
                              @RequestParam(defaultValue = "-1") String description,
                              @RequestParam(defaultValue = "-1") String endtime,
-                             @RequestParam(defaultValue = "-1") String startingbid,
+                             @RequestParam(defaultValue = "-1") String startingprice,
                              @RequestParam(defaultValue = "-1") String picture){
         int enabled = 1;
 
-        Item item = new Item(name,description,Integer.parseInt(startingbid),endtime,enabled,picture);
+        Item item = new Item(name,description,Integer.parseInt(startingprice),endtime,enabled,picture);
         List<User> users = (List<User>) userRepository.findAll();
         System.out.println(name);
         //Find the user that adds the blog
         System.out.println(description);
-        System.out.println(startingbid);
+        System.out.println(startingprice);
 
         System.out.println(endtime);
 
         System.out.println(enabled);
         System.out.println(picture);
-
+        //User loggedInUser = userRepository.findByEmail(MainController.getLoggedInUser("admin"));
 
 
         for(User a:users){
             System.out.println(a.getName());
-            if(a.getName().equals("User")){
+            if(a.getName().equals("Adminsson")){
                 item.setUser(a);
                 a.addItem(item);
 
