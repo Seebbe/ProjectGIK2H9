283022909da820b015339a21e51f959e68cc67e6
Marcus <massus@outlook.com>
2021-03-15 11:55:02 +0100

    Fixade så att användare läggs till automatiskt en gång per gång servern startas.

--- Auction/src/main/java/com/example/demo/controllers/MainController.java
+++ Auction/src/main/java/com/example/demo/controllers/MainController.java
@@ -3,6 +3,8 @@ package com.example.demo.controllers;
 import com.example.demo.models.User;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,9 @@ public class MainController {
     @Autowired
     private UserRepository userRepository;
 
-@RequestMapping("/userinit")
-    public String home(Model model){
+    @EventListener(ApplicationReadyEvent.class)
+    public String runOnStart(Model model){
+        //Skapar upp användare
     User u1 = new User("Admin","admin@admin.se","$2y$12$2tYyMIWKcov.yX/9TSmzcenjngVGK4UAQZ4AAIobRIiSsbbo7CLOe","ROLE_ADMIN");
     User u2 = new User("User","user@user.com","$2y$12$2tYyMIWKcov.yX/9TSmzcenjngVGK4UAQZ4AAIobRIiSsbbo7CLOe","ROLE_USER");
     User u3 = new User("User2","user2@user2.com","$2y$12$2tYyMIWKcov.yX/9TSmzcenjngVGK4UAQZ4AAIobRIiSsbbo7CLOe","ROLE_USER");
