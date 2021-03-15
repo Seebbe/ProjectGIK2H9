a9ddc88e86b0d48fbc1cb0203cf3db93fbe4b44c
Marcus <massus@outlook.com>
2021-03-15 11:26:20 +0100

    Gjorde klart alla repositories för models

--- Auction/src/main/java/com/example/demo/repositories/CompletedAuctionRepository.java
+++ Auction/src/main/java/com/example/demo/repositories/CompletedAuctionRepository.java
@@ -1,10 +1,10 @@
 package com.example.demo.repositories;
 
 import com.example.demo.models.CompletedAuction;
-import com.example.demo.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemRepository extends JpaRepository<Item, Integer> {
+public interface CompletedAuctionRepository extends JpaRepository <CompletedAuction, Integer> {
+
 }
